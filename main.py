0553049cc06b226a7bab774851dd5c80df909125
lovxonvy <87499021+triennale@users.noreply.github.com>
2022-06-13 20:56:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import os
+
+def getEmail():
+	URL_Email = "https://10minutemail.net/"
+	email_page = requests.get(URL_Email)
+	handle_email = BeautifulSoup(email_page.content, 'html.parser')
+	email = handle_email.find(class_='div-m-0 text-c')
+	email = str(email)[84:].replace("\"/></div>", '')
+	return email
+
+def clear(): 
+	if os.name == 'nt':
+		os.system('cls') 
+	else:
+		os.system('clear')
+
+def banner():
+	clear()
+	
+
+def writeToFile(email):
+	file = open("emails.txt", 'a')
+	write = ''
+	write += email + '\n'
+	file.write(write)
+	file.close()
+
+def main():
+	banner()
+	try:
+		numEmails = int(input("[+] quante ne vuoi creare?: "))
+	except:
+		print("[!] Invalid Value Entered. Restarting in 2 secs...")
+		time.sleep(2)
+		main()
+	writeOrNot = str(input("[+] vuoi salvarle in un file? (Y/N): "))
+	write = False
+	if 'y' == writeOrNot[0].lower():
+		write = True
+	if write == True:
+		print("[*] Writing to file has been enabled!")
+	print("[*] Creating {} emails".format(numEmails))
+	try:
+		for x in range(numEmails):
+			email = getEmail()
+			if '@' not in email:
+				print("""
+					[!] IP BANNATO USA UNA VPN O RESTARTA IL ROUTER 10MINUTEMAIL.NET.
+						CAMBIA IP CON UNA VPN O RESTARTA IL ROUTER.
+					""")
+				break
+			else:
+				if write == True:
+					writeToFile(email)
+				print("Email number {} : {}".format(x+1, email))
+	except:
+		print("Exiting...")
+		exit()
+if __name__ == "__main__":
+	main()
